9edc2419b60a8dd77a9260a3e8549034a6afc1f0
Per Lundberg <per.lundberg@ecraft.com>
2013-11-17 22:08:39 +0200

    Added simple .NET wrapper, with very little content thus far.

---
+++ MRuby.Net/Native/Mrb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MRuby.Net.Native
+{
+    public static class Mrb
+    {
+        // mrb_state* mrb_open(void);
+        [DllImport("mruby", EntryPoint = "mrb_open", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr Open();
+
+        // void mrb_close(mrb_state*);
+        [DllImport("mruby", EntryPoint = "mrb_close", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Close(IntPtr env);
+    }
+}
