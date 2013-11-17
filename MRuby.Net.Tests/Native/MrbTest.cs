6dbd0a593876003d14bdac635f3a23146ada91cb
Per Lundberg <per.lundberg@ecraft.com>
2013-11-17 22:09:17 +0200

    Added the mandatory unit test project. :)

---
+++ MRuby.Net.Tests/Native/MrbTest.cs
@@ -0,0 +1,15 @@
+﻿using MRuby.Net.Native;
+using Xunit;
+
+namespace MRuby.Net.Tests.Native
+{
+    public class MrbTest
+    {
+        [Fact]
+        public void Can_Close_an_environment_previously_openeded_with_Open()
+        {
+            var env = Mrb.Open();
+            Mrb.Close(env);
+        }
+    }
+}
