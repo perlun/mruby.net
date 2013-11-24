e788e0f76d949c15c25a7e26a238274d08bcd264
Per Lundberg <per.lundberg@ecraft.com>
2013-11-24 23:46:41 +0200

    Added MRuby.Net test project, and a (currently failing) test, for some functionality which is yet to be implemented.

---
+++ MRuby.Net.Tests/MRubyEnvironmentTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace MRuby.Net.Tests
+{
+    public class MRubyEnvironmentTest
+    {
+        [Fact]
+        public void Evaluate_CanCastResultToInt()
+        {
+            using (var mruby = new MRubyEnvironment())
+            {
+                dynamic result = mruby.Evaluate("10 + 20");
+                var intResult = (int?)result;
+                Assert.Equal(30, intResult);
+            }
+        }
+    }
+}
